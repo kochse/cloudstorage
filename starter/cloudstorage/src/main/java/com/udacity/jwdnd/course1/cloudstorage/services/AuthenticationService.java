e65eacb77aa9bce253fb7d7c3bffdf0808014ab1
Sebastian <sebastian@Sebastians-Mac-mini.local>
2021-07-03 16:45:19 +0200

    init

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/AuthenticationService.java
@@ -0,0 +1,39 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+@Service
+public class AuthenticationService implements AuthenticationProvider {
+    private UserMapper userMapper;
+    private HashService hashService;
+
+    public AuthenticationService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    @Override
+    public Authentication authentication(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        User user = userMapper.getUser(username);
+        if (user != null) {
+            String encodeSalt = user.getSalt();
+            String hashedPassword = hashService.getHashedValue(password, encodeSalt);
+            if (user.getPassword().equals(hashedPassword)) {
+                return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+            }
+        }
+
+        return null;
+    }
+}
