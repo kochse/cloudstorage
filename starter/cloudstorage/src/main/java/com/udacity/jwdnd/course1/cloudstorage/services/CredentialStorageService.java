ece92d39f609b08bc536a343b3f1f6fb4cee9f37
Sebastian Koch <kochsebastian@gmail.com>
2021-11-27 15:12:40 +0100

    add encryp password

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialStorageService.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/CredentialStorageService.java
@@ -4,20 +4,44 @@ import com.udacity.jwdnd.course1.cloudstorage.mapper.CredentialMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.Credential;
 import org.springframework.stereotype.Component;
 
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.stream.Stream;
+
 @Component
 public class CredentialStorageService {
 
     private final CredentialMapper credentialMapper;
+    private final EncryptionService encryptionService;
 
-    public CredentialStorageService(CredentialMapper credentialMapper) {
+    private String encodedKey;
+
+    public CredentialStorageService(CredentialMapper credentialMapper, EncryptionService encryptionService) {
         this.credentialMapper = credentialMapper;
+        this.encryptionService = encryptionService;
+
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[16];
+        random.nextBytes(key);
+        this.encodedKey = Base64.getEncoder().encodeToString(key);
     }
 
     public Credential[] getCredentials(Integer userId) {
-        return this.credentialMapper.readAll(userId);
+        Stream<Credential> credentials = Arrays.stream(this.credentialMapper.readAll(userId));
+        return credentials.map(credential -> {
+            if (credential.getPassword() != null) {
+                String password = encryptionService.decryptValue(credential.getPassword(), this.encodedKey);
+                credential.setPassword(password);
+            }
+           return credential;
+        }).toArray(Credential[]::new);
     }
 
     public void saveCredential(Credential credential) {
+        String encryptedPassword = encryptionService.encryptValue(credential.getPassword(), this.encodedKey);
+        credential.setPassword(encryptedPassword);
+
         if (credential.getCredentialId() != null) {
             this.credentialMapper.update(credential);
         } else {
